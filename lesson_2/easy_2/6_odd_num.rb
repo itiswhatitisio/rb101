ca70be5fa30fe93314a21e137e9d8f4582102caa
itiswhatitisio <37877101+itiswhatitisio@users.noreply.github.com>
2019-10-24 15:16:55 +0200

    complete exercise 6

--- lesson_2/easy_2/6_odd_num.rb
+++ lesson_2/easy_2/6_odd_num.rb
@@ -4,3 +4,11 @@ while i <= 99 do
   puts i
   i+=2
 end
+
+# using upto method
+
+1.upto(99) { |num| puts num if num.odd? }
+
+# using Array.select method
+
+(1..99).select { |num| puts num if num.odd? }

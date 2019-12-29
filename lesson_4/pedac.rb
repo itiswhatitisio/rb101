2b7ab11fe8f892ffeb83f83003f717b957c15284
Tonya <a.khodzhaeva@gmail.com>
2019-12-29 12:16:21 +0000

    complete easy_3 set from small problems

---
+++ lesson_4/pedac.rb
@@ -0,0 +1,25 @@
+# PROBLEM:
+
+# Given a string, write a method `palindrome_substrings` which returns
+# all the substrings from a given string which are palindromes. Consider
+# palindrome words case sensitive.
+
+# Test cases:
+
+# palindrome_substrings("supercalifragilisticexpialidocious") == ["ili"]
+# palindrome_substrings("abcddcbA") == ["bcddcb", "cddc", "dd"]
+# palindrome_substrings("palindrome") == []
+# palindrome_substrings("") == []
+
+
+
+# Input: string
+
+# Output: array of substrings
+
+# Rules:
+
+# Explicit requirements: 
+# - substrings must be palindromes (a palindrome is a word that reads
+# the same forwards and backward)
+# - substrings are case sensitive (Aba is not a palindrome)

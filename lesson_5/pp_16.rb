00f97b43892f4219333ee8bc0cfe5c88b5447ea4
Tonya <a.khodzhaeva@gmail.com>
2020-03-28 16:25:36 +0000

    changes to lesson_5

--- lesson_5/pp_16.rb
+++ lesson_5/pp_16.rb
@@ -21,4 +21,17 @@ f65c57f6-a6aa-17a8-faa1-a67f2dc9fa91
 
 =end
 
+def generate_uuid
+  characters = %w(0 1 2 3 4 5 6 7 8 9 a b c d e f)
+  arr = [8, 4, 4, 4, 12]
+  uuid = ''
 
+  arr.each_with_index do |el, idx|
+    el.times { uuid += characters.sample }
+    uuid += '-' unless idx >= arr.size - 1
+  end
+  uuid
+
+end
+
+p generate_uuid
